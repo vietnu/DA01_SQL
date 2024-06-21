13eb507705f55ba018c205366a97fac9c1adbf0f
vietnu <cunpong2000@gmail.com>
2024-06-21 16:38:28 +0700

    Update DAY21_Project2.sql

--- DAY21_Project2.sql
+++ DAY21_Project2.sql
@@ -8,15 +8,29 @@ where FORMAT_DATE('%Y-%m', created_at) between '2019-01'and'2022-04'
 group by FORMAT_DATE('%Y-%m', created_at)
 order by date
 
+--đúng
+  select  
+FORMAT_DATE('%Y-%m', t2.delivered_at) as date,
+count(distinct t1.user_id) as total_user,
+sum(case when t1.status like 'Complete'then 1 else 0 end)as total_orde
+from bigquery-public-data.thelook_ecommerce.orders as t1
+Join bigquery-public-data.thelook_ecommerce.order_items as t2 
+on t1.order_id=t2.order_id
+where FORMAT_DATE('%Y-%m', t2.delivered_at) between '2019-01'and'2022-04'
+and t1.status='Complete'
+group by FORMAT_DATE('%Y-%m', t2.delivered_at)
+order by date
+
 --2. Giá trị đơn hàng trung bình (AOV) và số lượng khách hàng mỗi tháng
 select  
 FORMAT_DATE('%Y-%m', created_at) as date,
 count(distinct user_id) as distinct_users,
-sum(sale_price)/count(order_id) as average_order_value
+sum(sale_price)/count(distinct order_id) as average_order_value
 from bigquery-public-data.thelook_ecommerce.order_items
 where FORMAT_DATE('%Y-%m', created_at) between '2019-01'and'2022-04'
 group by FORMAT_DATE('%Y-%m', created_at)
 order by date
+
 
 --3. Nhóm khách hàng theo độ tuổi
 select first_name,last_name, gender, age,'youngest'as tag
@@ -37,21 +51,28 @@ where rnk =1
 with cte as (
 select*,dense_rank()over(partition by date order by profit desc) as rnk
 from(
-select FORMAT_DATE('%Y-%m', created_at) as date, product_id,name, sale_price, cost,
-sum(sale_price)over(partition by product_id)-sum(cost)over(partition by product_id) as profit,
+select FORMAT_DATE('%Y-%m', delivered_at) as date, product_id,name,
+ sum(sale_price) as sales, sum(cost) as cost,
+sum(sale_price)-sum(cost) as profit,
 from bigquery-public-data.thelook_ecommerce.order_items as a
 inner join bigquery-public-data.thelook_ecommerce.products as b
 on a.id=b.id
-where status like 'Complete') as a)
+where status ='Complete'
+Group by date,a.product_id, b.name
+order by date) as a)
 select * from cte
 where rnk between 1 and 5
---5.
-select DATE(created_at) as date,category,
-sum(sale_price)over(partition by category) as revenue
-from bigquery-public-data.thelook_ecommerce.order_items as a
-inner join bigquery-public-data.thelook_ecommerce.products as b
-on a.id=b.id
-where DATE(created_at) between '2022-01-01'and'2022-04-15'
 order by date
+--5.
+select DATE(delivered_at ) as date,category,
+  round(sum(sale_price),2) as revenue,
+  from bigquery-public-data.thelook_ecommerce.order_items as a
+  inner join bigquery-public-data.thelook_ecommerce.products as b
+  on a.product_id=b.id
+  where delivered_at BETWEEN '2022-01-15 00:00:00' AND '2022-04-16 00:00:00'
+  and status='Complete' 
+  group by date,category
+  order by date
+
 
 
